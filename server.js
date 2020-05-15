573f1a0e35deeba0a40d5427bd118b15a89164ca
ShubhamDuggal <42518566+ShubhamDuggal@users.noreply.github.com>
2020-05-15 12:05:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,68 @@
-var express = require('express');
+var http = require('http');
+querystring = require('querystring');
+const JustWatch = require('./');
+var port = process.env.PORT || 3000;
 
-var app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+
+const server = http.createServer((request, response) => {
+	response.setHeader('Access-Control-Allow-Origin', '*')
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+      	response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+               
+if(request.method === 'POST') {
+        let body = '';
+        
+        // very important to handle errors
+        request.on('error', (err) => {
+            if(err) {
+                response.writeHead(500, {'Content-Type': 'text/html'});
+	        response.write('An error occurred');
+                response.end();
+            }
+        });
+        
+        // read chunks of POST data
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+
+        // when complete POST data is received
+        request.on('end', () => {
+            // use parse() method
+            body = querystring.parse(body);
+			response.writeHead(200, {'Content-Type': 'application/json'});
+            //   response.writeHead(200, {'Content-Type': 'text/html'});
+          //  response.write('404 Not Found\n');
+            
+            // { name: 'John', gender: 'MALE', email: 'john@gmail.com' }
+            //console.log(body);
+             response.write(JSON.stringify(body, null, 4));
+          //  var justwatch = new JustWatch();
+
+	//var searchResult = justwatch.search({query: body});
+	// return searchResult;
+          response.end();
+        });
+    }
+    
+    // rest of the code
 });
 
-app.get('/', function (req, res) {
-  var data = {
-    "bestAnimals": [
-      "wombat",
-      "corgi",
-      "puffer fish",
-      "owl",
-      "crow"
-    ]
-  };
-res.writeHead(200, {'Content-Type': 'application/json'});
-  res.json(data);
-});
+server.listen(port);
 
 
-var server = app.listen(3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+function print_result(result) {
+  return function(request, response) {
+    //  response.writeHeader(200, {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"});
+      response.setHeader("Access-Control-Allow-Origin", "*");
+      response.setHeader("Access-Control-Allow-Credentials", "true");
+      response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+      response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+	  response.write(JSON.stringify(result, null, 4));
+      response.end();
+  }
+}
 
-});
