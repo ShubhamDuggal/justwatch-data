07af34e977c55989da3c6674ba1502919af9b069
ShubhamDuggal <42518566+ShubhamDuggal@users.noreply.github.com>
2020-05-15 10:57:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ app.use(bodyParser.json());
 
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', origin || "*");
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+     res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
