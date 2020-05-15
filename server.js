75659a8df25689b54fdef02bc8bb8ccf694657f6
ShubhamDuggal <42518566+ShubhamDuggal@users.noreply.github.com>
2020-05-15 10:13:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ if(request.method === 'POST') {
         request.on('error', (err) => {
             if(err) {
                 response.writeHead(500, {'Content-Type': 'text/html'});
+		     response.setHeader("Access-Control-Allow-Origin", "*");
+     		response.setHeader("Access-Control-Allow-Credentials", "true");
+      		response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+      		response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+               
 	        response.write('An error occurred');
                 response.end();
             }
