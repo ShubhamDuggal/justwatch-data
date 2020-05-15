e24bbbef617f074d2991d0ac7f9da712e7a0e320
ShubhamDuggal <42518566+ShubhamDuggal@users.noreply.github.com>
2020-05-15 10:46:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var express        =         require("express");
-var bodyParser     =         require("body-parser");
-var app            =         express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
 app.post('/',function(req,res){
-  var user_name=req.body.name;
-  res.send("test")
-  res.end("yes");
+ // var user_name=req.body.name;
+    console.log('receiving data...');
+    console.log('body is ',req.body);
+    res.send(req.body);
 });
 app.listen(3000,function(){
   console.log("Started on PORT 3000");
