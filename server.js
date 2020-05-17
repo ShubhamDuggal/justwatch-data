40175653dd99e955c6cac8414f0c520b87b865c5
ShubhamDuggal <42518566+ShubhamDuggal@users.noreply.github.com>
2020-05-17 18:00:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const bodyParser = require('body-parser');
 const url = require('url');
 const querystring = require('querystring');
 
+
 let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', async function(req, res) {
 var jsonObj = {'query': req.query.name};
+	res.setHeader('Content-Type', 'application/json');
 	res.send(jsonObj);
   
 });
