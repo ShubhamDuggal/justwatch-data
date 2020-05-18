caca61afeb498a0c5698695d07bf5d7e87dae32c
ShubhamDuggal <42518566+ShubhamDuggal@users.noreply.github.com>
2020-05-18 22:42:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var http = require('http');
 querystring = require('querystring');
 const JustWatch = require('./');
 var port = process.env.PORT || 3000;
-var fs = require('fs');
 
 const server = http.createServer((request, response) => {
 	response.setHeader('Access-Control-Allow-Origin', '*')
@@ -17,7 +16,7 @@ if(request.method === 'POST') {
         // very important to handle errors
         request.on('error', (err) => {
             if(err) {
-                response.writeHead(500, {'Content-Type': 'text/html'});
+                response.writeHead(500, {'Content-Type': 'application/json'});
 	        response.write('An error occurred');
                 response.end();
             }
@@ -33,18 +32,20 @@ if(request.method === 'POST') {
             // use parse() method
 		
 		jsonData = JSON.parse(body);
-             var name= jsonData.name;
-			 
-	//		 var jsonObj = {'query': jsonData.name};
-		//var searchResult = justwatch.search({query: name});
-        //     response.writeHead(200, {'Content-Type': 'application/json'});
-       //     response.write(JSON.stringify(jsonObj, null, 4)); 
-		
-		
-	 var justwatch = new JustWatch();
-         var searchResult = await justwatch.search({query: name});
-	print_result(searchResult);
-          response.end();
+             var name = jsonData.name;
+	
+	(async function(){
+	var justwatch = new JustWatch();
+
+	var searchResult = await justwatch.search({query: name});
+	 // response.writeHeader(200, {"Content-Type": "application/json"});
+      response.write(JSON.stringify(searchResult, null, 4));
+ response.end();
+	//var episodes = await justwatch.getEpisodes(searchResult.items[0].id);
+//	print_result("episodes", episodes);
+})();
+
+	        
         });
     }
     
@@ -53,18 +54,3 @@ if(request.method === 'POST') {
 
 server.listen(port);
 
-
-
-
-function print_result(result) {
-  return function(request, response) {
-    //  response.writeHeader(200, {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"});
-      response.setHeader("Access-Control-Allow-Origin", "*");
-      response.setHeader("Access-Control-Allow-Credentials", "true");
-      response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-      response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-	    response.writeHead(200, {'Content-Type': 'application/json'});
-	  response.write(JSON.stringify(result, null, 4));
-      response.end();
-  }
-}
